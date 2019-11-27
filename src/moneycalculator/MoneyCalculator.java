288d3517b99ccaccf14bd9d175938bbe194ee3b1
COCO <COCO@DESKTOP-OQERUVD>
2019-11-27 18:08:09 +0000

    Versión 1(Spaghetti) de MoneyCalculator

---
+++ src/moneycalculator/MoneyCalculator.java
@@ -0,0 +1,31 @@
+package moneycalculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class MoneyCalculator {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Introduzca una cantidad en dólares: ");
+        Scanner scanner = new Scanner(System.in);
+        double amount = Double.parseDouble(scanner.next());
+        double exchangeRate = getExchangeRaye("USD", "EUR");
+        System.out.println(amount + " USD equivalen a " + amount*exchangeRate + "EUR");        
+    }
+    
+    private static double getExchangeRaye(String from, String to) throws IOException{
+        URL url = new URL("http://free.currencyconverterapi.com/api/v5/convert?q="
+                + from + '_' + to + "&compact=ultra&apiKey=cfba020bc2a17499ee7e");
+        
+        URLConnection connection = url.openConnection();
+        try (BufferedReader reader = 
+                new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+            String line = reader.readLine();
+            String line1 = line.substring(line.indexOf(':')+1, line.indexOf('}'));
+            return Double.parseDouble(line1);
+        }
+    }
+}
